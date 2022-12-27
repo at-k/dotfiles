fa6a841ad8c6e9150c2b5e93ff84b5dc18100dc0
Atsushi Kawamura <akawamura@c-fo.com>
2022-12-28 00:58:57 +0900

    add alias

--- .config/zsh/aws.zsh
+++ .config/zsh/aws.zsh
@@ -6,3 +6,5 @@ alias a='aws'
 
 alias aad='aws autoscaling describe-auto-scaling-groups'
 alias aad-fn='(){aws autoscaling describe-auto-scaling-groups --filters="Name=tag:Name,Values=$1"}'
+
+alias aeu='(){aws eks update-kubeconfig --name $1}'
