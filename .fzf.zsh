3582769c69a3e4484a941f1da8b131ce29cc5602
Atsushi Kawamura <akawamura@c-fo.com>
2023-02-19 19:09:52 +0900

    fzf history

--- .fzf.zsh
+++ .fzf.zsh
@@ -46,4 +46,10 @@ _fzf_comprun() {
   esac
 }
 
+function select-history() {
+  BUFFER=$(history -n -r 1 | fzf --no-sort +m --query "$LBUFFER" --prompt="History > ")
+  CURSOR=$#BUFFER
+}
+zle -N select-history
+bindkey '^r' select-history
 
